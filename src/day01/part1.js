719f1cfc9fc27bd828eaae4c3655e6952c3d9656
Lucas-ez <lucasezequiel237@gmail.com>
2022-12-01 16:39:30 -0300

    Día 1

--- src/day01/part1.js
+++ src/day01/part1.js
@@ -1,5 +1,16 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input) {
-  return 0;
+  
+  const array = input.split('\r\n')
+  const elfos = [0]
+
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === '')
+      elfos.push(0)
+    else
+      elfos[elfos.length - 1] += parseInt(array[i])
+  }
+
+  return Math.max(...elfos);
 }
--- src/day01/part2.js
+++ src/day01/part2.js
@@ -1,5 +1,33 @@
 // Advent of Code - Day 1 - Part Two
 
 export function part2(input) {
-  return 0;
+  const array = input.split('\r\n')
+  const elfos = [0]
+
+  for( let i = 0; i < array.length; i++ ) {
+    if(array[i] === '')
+      elfos.push(0)
+    else
+      elfos[elfos.length - 1] += parseInt(array[i])
+  }
+
+  //hacer mi math.max
+
+  const mayores = [0, 0, 0]
+
+  for( let i = 0; i < elfos.length; i++ ) {
+    if(elfos[i] > mayores[0]) {
+      mayores[2] = mayores[1]
+      mayores[1] = mayores[0]
+      mayores[0] = elfos[i]
+    } else if(elfos[i] > mayores[1]) {
+      mayores[2] = mayores[1]
+      mayores[1] = elfos[i]
+    } else if(elfos[i] > mayores[2]) {
+      mayores[2] = elfos[i]
+    }
+  }
+
+
+  return mayores.reduce( (acum, value) => acum + value, 0);
 }
