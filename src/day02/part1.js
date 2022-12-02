d5bfa937850d772623c47072ec6c87058a6fb310
Lucas-ez <lucasezequiel237@gmail.com>
2022-12-02 03:29:23 -0300

    Add Día 2

--- src/day02/part1.js
+++ src/day02/part1.js
@@ -1,5 +1,36 @@
 // Advent of Code - Day 2 - Part One
 
+/*
+  First column -> oponnent play
+  A: Rock, B: Paper, C: Scissors
+
+  Second column -> my play
+  X: Rock, Y: Paper, Z: Scissors
+
+  Shape Score: (1 for Rock, 2 for Paper, and 3 for Scissors)
+  Plus Score: (0 if you lost, 3 if the round was a draw, and 6 if you won)
+
+  Round Score = Shape Score + Plus Score
+*/
+
 export function part1(input) {
-  return 0;
+  const rounds = input.split('\r\n')
+  const scores = rounds.map( strRound => {
+    const [oponnentPlay, myPlay] = strRound.split(" ")
+    let score = myPlay === 'X' ? 1 : ( myPlay === 'Y' ) ? 2 : 3
+
+    if ((myPlay === 'X' && oponnentPlay === 'A') || 
+        (myPlay === 'Y' && oponnentPlay === 'B') || 
+        (myPlay === 'Z' && oponnentPlay === 'C'))
+      score += 3
+
+    if ((myPlay === 'X' && oponnentPlay === 'C') || 
+        (myPlay === 'Y' && oponnentPlay === 'A') || 
+        (myPlay === 'Z' && oponnentPlay === 'B'))
+      score += 6
+
+    return score
+  })
+
+  return scores.reduce((prev, current) => prev + current, 0);
 }
--- src/day02/part2.js
+++ src/day02/part2.js
@@ -1,5 +1,29 @@
 // Advent of Code - Day 2 - Part Two
 
 export function part2(input) {
-  return 0;
+  const rounds = input.split('\r\n')
+  const scores = rounds.map( strRound => {
+    const [oponnentPlay, roundResult] = strRound.split(" ")
+    let score = roundResult === 'X' ? 0 : ( roundResult === 'Y' ) ? 3 : 6
+
+    // Shape Score: (1 for Rock, 2 for Paper, and 3 for Scissors)
+    if ((roundResult === 'X' && oponnentPlay === 'B') || 
+        (roundResult === 'Y' && oponnentPlay === 'A') || 
+        (roundResult === 'Z' && oponnentPlay === 'C'))
+      score += 1
+
+    if ((roundResult === 'X' && oponnentPlay === 'C') || 
+        (roundResult === 'Y' && oponnentPlay === 'B') || 
+        (roundResult === 'Z' && oponnentPlay === 'A'))
+      score += 2
+
+    if ((roundResult === 'X' && oponnentPlay === 'A') || 
+        (roundResult === 'Y' && oponnentPlay === 'C') || 
+        (roundResult === 'Z' && oponnentPlay === 'B'))
+      score += 3
+
+    return score
+  })
+
+  return scores.reduce((prev, current) => prev + current, 0);
 }
